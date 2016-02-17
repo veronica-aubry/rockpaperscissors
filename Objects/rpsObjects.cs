730fb42d5fc6bf68a9c1a6b9db927fa23dbcb355
veronica alley <veronica.aubry.alley@gmail.com>
2016-02-17 10:28:43 -0800

    add passing specs for all options and functioning win determiner

--- Objects/rpsObjects.cs
+++ Objects/rpsObjects.cs
@@ -11,6 +11,7 @@ namespace rpsObjects {
     private string _player2Input;
     private int _player1InputInt;
     private int _player2InputInt;
+    private string winningMove;
 
     public static Dictionary<string,int> _optionScores = new Dictionary<string,int>
     {
@@ -28,35 +29,36 @@ namespace rpsObjects {
     };
 
     public Game (string player1Input, string player2Input) {
-      _player1Input = player1Input;
-      _player2Input = player2Input;
+      _player1Input = player1Input.ToLower();
+      _player2Input = player2Input.ToLower();
     }
 
     public string RPSWinner(){
+
 
       if (_player1Input == _player2Input) {
         return "draw";
       }else {
         foreach (KeyValuePair<string,int> options in _optionScores) {
           if (options.Key == _player1Input) {
-            int _player1InputInt = options.Value;
+            _player1InputInt = options.Value;
           }
           if (options.Key == _player2Input) {
-            int _player2InputInt = options.Value;
+             _player2InputInt = options.Value;
           }
         }
         int gameTotal = _player1InputInt + _player2InputInt;
-        foreach (KeyValuePair<string,int> options in _optionWins) {
+        foreach (KeyValuePair<int,string> options in _optionWins) {
           if (options.Key == gameTotal) {
-            string winningMove = options.Value;
-            System.Console.WriteLine(winningMove);
+            winningMove = options.Value;
           }
         }
         if (_player1Input == winningMove) {
           return "Player 1 Wins!!";
-        }else {
+        }else if (_player2Input == winningMove){
           return "Player 2 Wins!!";
         }
+        return "ya done fucked up";
       }
     }
   }
--- Objects/rpsTest.cs
+++ Objects/rpsTest.cs
@@ -7,16 +7,47 @@ namespace  rpsObjects
   public class rpsTest
   {
     [Fact]
-    public void RPSWinner_ForDraw_returnFalse()
+    public void RPSWinner_ForDraw_returnP2_and_P1_DRAW()
     {
       Game newGame = new Game("paper", "paper");
       Assert.Equal("draw", newGame.RPSWinner());
     }
+
     [Fact]
-    public void RPSWinner_For_returnFalse()
+    public void RPSWinner_ForRockPaper_returnP2Wins_paper()
     {
       Game newGame = new Game("rock", "paper");
       Assert.Equal("Player 2 Wins!!", newGame.RPSWinner());
+    }
+    [Fact]
+    public void RPSWinner_For_returnP1Wins_paper()
+    {
+      Game newGame = new Game("PAPer", "rock");
+      Assert.Equal("Player 1 Wins!!", newGame.RPSWinner());
+    }
+    [Fact]
+    public void RPSWinner_For_returnP2Wins_scissors()
+    {
+      Game newGame = new Game("paper", "scissors");
+      Assert.Equal("Player 2 Wins!!", newGame.RPSWinner());
+    }
+    [Fact]
+    public void RPSWinner_For_returnP1Wins_scissors()
+    {
+      Game newGame = new Game("scissors", "paper");
+      Assert.Equal("Player 1 Wins!!", newGame.RPSWinner());
+    }
+    [Fact]
+    public void RPSWinner_For_returnP2Wins_Rock()
+    {
+      Game newGame = new Game("scissors", "rock");
+      Assert.Equal("Player 2 Wins!!", newGame.RPSWinner());
+    }
+    [Fact]
+    public void RPSWinner_For_returnP1Wins_Rock()
+    {
+      Game newGame = new Game("rock", "scissors");
+      Assert.Equal("Player 1 Wins!!", newGame.RPSWinner());
     }
   }
 }
